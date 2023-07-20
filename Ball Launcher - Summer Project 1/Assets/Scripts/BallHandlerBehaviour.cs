8b896a0a74184ae2ea0d81aab4ad1ddb575600af
Valeska <valeskabagaric@hotmail.com>
2023-07-19 22:13:00 -0600

    Implemented Multi-Touch for testing

--- Ball Launcher - Summer Project 1/Assets/Scripts/BallHandlerBehaviour.cs
+++ Ball Launcher - Summer Project 1/Assets/Scripts/BallHandlerBehaviour.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class BallHandlerBehaviour : MonoBehaviour
 {
@@ -20,13 +23,22 @@ public class BallHandlerBehaviour : MonoBehaviour
         mainCamera = Camera.main;
         SpawnBall();
     }
+
+    private void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+    }
+
+    private void OnDisable()
+    {
+        EnhancedTouchSupport.Disable();
+    }
+
     void Update()
     {
         if (currentBallRigidBody == null) {return;}
-        {
-            
-        }
-        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        
+        if (Touch.activeTouches.Count == 0)
         {
             if (isDragging)
             {
@@ -40,9 +52,16 @@ public class BallHandlerBehaviour : MonoBehaviour
         isDragging = true;
         
         currentBallRigidBody.isKinematic = true;
-        
-        Vector2 touchPosition =Touchscreen.current.primaryTouch.position.ReadValue();
 
+        Vector2 touchPosition = new Vector2();
+
+        foreach (Touch touch in Touch.activeTouches)
+        {
+            touchPosition += touch.screenPosition;
+        }
+
+        touchPosition /= Touch.activeTouches.Count;
+        
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
 
         currentBallRigidBody.position = worldPosition;
