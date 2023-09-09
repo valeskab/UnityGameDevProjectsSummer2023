f8edd992eb561c82ff4b66cb8b8950fef164dc60
Valeska Bagaric <valeskabagaric@hotmail.com>
2023-09-08 23:32:22 -0600

    finished high score system in main menu

--- Simple Driving - Summer Project 2/Assets/Scripts/MainMenu.cs
+++ Simple Driving - Summer Project 2/Assets/Scripts/MainMenu.cs
@@ -1,8 +1,18 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private TMP_Text highScoreText;
+
+    private void Start()
+    {
+        int highScore = PlayerPrefs.GetInt(ScoreHandlerBehaviour.HighScoreKey, 0);
+
+        highScoreText.text = $"High Score: {highScore}";
+    }
+
     public void Play()
     {
         SceneManager.LoadScene(1);
--- Simple Driving - Summer Project 2/Assets/Scripts/ScoreHandlerBehaviour.cs
+++ Simple Driving - Summer Project 2/Assets/Scripts/ScoreHandlerBehaviour.cs
@@ -9,14 +9,23 @@ public class ScoreHandlerBehaviour : MonoBehaviour
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private float scoreMultiplier;
 
+    public const string HighScoreKey = "HighScore";
+
     private float score;
-    
-    
-    
+
     void Update()
     {
         score += Time.deltaTime * scoreMultiplier;
 
         scoreText.text = Mathf.FloorToInt(score).ToString();
+    }
+    
+    private void OnDestroy()
+    {
+        int currentHighScore = PlayerPrefs.GetInt(HighScoreKey, 0);
+        if (score > currentHighScore)
+        {
+            PlayerPrefs.SetInt(HighScoreKey, Mathf.FloorToInt(score));
+        }
     }
 }
